180b2ef244950138b3572ec9915688d8f48f2b02
李顺仪 <lishunyi0109@gmail.com>
2020-04-29 17:55:46 +0800

    :tada: 初始提交

---
+++ src/main/java/com/lishunyi/pay/LishunyiPayApplication.java
@@ -0,0 +1,13 @@
+package com.lishunyi.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LishunyiPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LishunyiPayApplication.class, args);
+	}
+
+}
